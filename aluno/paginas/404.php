45481dfde835bb502299ae93317507a5e97133df
Vinicius V Elias <vinicius_ve_@hotmail.com>
2019-05-27 22:17:53 -0300

    1

--- aluno/paginas/404.php
+++ aluno/paginas/404.php
@@ -1,1 +1,7 @@
-<h1> pagina de erro 404</h1>
+
+
+<div class="jumbotron" id="erro">
+<img src="../img/back2.png" aling="left" id="fter">  
+  <hr class="my-4">
+  <a class="btn btn-primary btn-lg" id="btt" href="home" role="button">Voltar</a>
+</div>
