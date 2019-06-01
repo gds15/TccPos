2f3b7f265a48e09dbcef4811478ef454312cb936
Vinicius V Elias <vinicius_ve_@hotmail.com>
2019-06-01 00:15:56 -0300

    PaginaAtividades

---
+++ aluno/paginas/atividade.php
@@ -0,0 +1,22 @@
+
+<div class="container">
+<ul class="nav nav-tabs" id="myTab" role="tablist">
+  <li class="nav-item">
+    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#Feitas" role="tab" 
+    aria-controls="Atividades Feitas" aria-selected="true">Atividades Feitas</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#Fazer" role="tab" 
+    aria-controls="Atividades a Fazer" aria-selected="false">Atividades a Fazer</a>
+  </li>
+ 
+</ul>
+<div class="tab-content" id="myTabContent">
+  <div class="tab-pane active" id="home" role="tabpanel" aria-labelledby="home-tab">
+    <h1>Atividades Feitas</h1>
+  
+  </div>
+  <div class="tab-pane" id="perfil" role="tabpanel" aria-labelledby="profile-tab">
+    <h1>Atividades a Fazer</h1>
+  </div>
+</div>
