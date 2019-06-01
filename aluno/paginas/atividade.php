a48e872168545fb8d3cc95f2f81db508956a52b5
Vinicius V Elias <vinicius_ve_@hotmail.com>
2019-05-31 21:31:47 -0300

    teste

--- aluno/paginas/atividade.php
+++ aluno/paginas/atividade.php
@@ -1,14 +1,2 @@
-
-    <div class="container">
-        <div class="ati">
-            <h1><strong>Lista de Atividades</strong></h1>
-
-            <ul class="nav nav-tabs">
-                <li class="nav-item">
-                    <a class="nav-link active" href="atif">Atividades Feitas</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="atiaf">Atividades A Fazer</a>
-                </li>
-            </ul>
+<label>Teste 1</label>
+<input name="teste" id="teste"  type="text" value="teste">
