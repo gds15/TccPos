43172ba7cb35ea71777dcf57d8795c8549bf2571
Vinicius V Elias <vinicius_ve_@hotmail.com>
2019-06-01 01:15:38 -0300

    AtividadeQProntas

--- aluno/paginas/atividade.php
+++ aluno/paginas/atividade.php
@@ -1,22 +1,98 @@
 
-<div class="container">
-<ul class="nav nav-tabs" id="myTab" role="tablist">
-  <li class="nav-item">
-    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#Feitas" role="tab" 
-    aria-controls="Atividades Feitas" aria-selected="true">Atividades Feitas</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#Fazer" role="tab" 
-    aria-controls="Atividades a Fazer" aria-selected="false">Atividades a Fazer</a>
-  </li>
- 
-</ul>
-<div class="tab-content" id="myTabContent">
-  <div class="tab-pane active" id="home" role="tabpanel" aria-labelledby="home-tab">
-    <h1>Atividades Feitas</h1>
-  
-  </div>
-  <div class="tab-pane" id="perfil" role="tabpanel" aria-labelledby="profile-tab">
-    <h1>Atividades a Fazer</h1>
-  </div>
+<div class="tatividades">
+  <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+            <br />
+            <h2>Atividades</h2>
+            
+            <ul class="nav nav-tabs">
+              <li class="nav-item">
+                <a href="#feitas" class="nav-link active" role="tab" data-toggle="tab">Atividades Feitas</a>
+              </li>
+
+              <li class="nav-item">
+                <a href="#fazer" class="nav-link" role="tab" data-toggle="tab">Atividades a Fazer</a>
+              </li>         
+            </ul>
+
+            <div class="tab-content">
+              <div role="tabpanel" class="tab-pane active" id="feitas">
+              <h4>Atividades Feitas</h4>
+
+
+                <div class="card" style="width: 18rem;" id="card">
+                  <img class="card-img-top" src=".../100px180/" alt="Imagem de capa do card">
+                  <div class="card-body">
+                    <h5 class="card-title">Tabuada</h5>
+                    <p class="card-text">TABUADA De 0 A 10</p>
+                    <a href="#" class="btn btn-primary">Ver Atividade</a>
+                  </div>
+                </div>
+
+                <div class="card" style="width: 18rem;" id="card">
+                  <img class="card-img-top" src=".../100px180/" alt="Imagem de capa do card">
+                  <div class="card-body">
+                    <h5 class="card-title">Marvel</h5>
+                    <p class="card-text">Texto sobre o universo Marvel</p>
+                    <a href="#" class="btn btn-primary">Ver Atividade</a>
+                  </div>
+                </div>
+
+                <div class="card" style="width: 18rem;" id="card">
+                  <img class="card-img-top" src=".../100px180/" alt="Imagem de capa do card">
+                  <div class="card-body">
+                    <h5 class="card-title">Brasil</h5>
+                    <p class="card-text">Historia de quem descobriu o Brasil</p>
+                    <a href="#" class="btn btn-primary">Ver Atividade</a>
+                  </div>
+                </div>           
+              
+              </div>
+
+
+              <div role="tabpanel" class="tab-pane" id="fazer">
+              <h4>Atividades a Fazer</h4>
+
+              <div class="card" style="width: 18rem;" id="card">
+                  <img class="card-img-top" src=".../100px180/" alt="Imagem de capa do card">
+                  <div class="card-body">
+                    <h5 class="card-title">Dissertação sobre a DC Comics</h5>
+                    <p class="card-text">Dissertação sobre a DC Comics</p>
+                    <a href="#" class="btn btn-primary">Fazer Atividade</a>
+                  </div>
+                </div>
+
+                <div class="card" style="width: 18rem;" id="card">
+                  <img class="card-img-top" src=".../100px180/" alt="Imagem de capa do card">
+                  <div class="card-body">
+                    <h5 class="card-title">Texto Sobre Power Ranges</h5>
+                    <p class="card-text">O inicio de tudo</p>
+                    <a href="#" class="btn btn-primary">Fazer Atividade</a>
+                  </div>
+                </div>
+
+                <div class="card" style="width: 18rem;" id="card">
+                  <img class="card-img-top" src=".../100px180/" alt="Imagem de capa do card">
+                  <div class="card-body">
+                    <h5 class="card-title">Como jogar CSGO sem estresse</h5>
+                    <p class="card-text">Tarefa impossivel de ser feita</p>
+                    <a href="#" class="btn btn-primary">Fazer Atividade</a>
+                  </div>
+                </div>
+
+              </div>
+
+              
+
+              
+                </div>
+            </div>
+          </div>
+      </div>
 </div>
+
+    <!-- jQuery first, then Bootstrap JS. -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/js/bootstrap.js"></script>
+  </body>
